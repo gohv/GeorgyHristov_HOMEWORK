61e021d5dfb6d80c257c2a167d870ad188298722
gohv <hristovsotirovgeorgi@gmail.com>
2016-02-24 16:46:14 +0200

    HomeworkExceptionsUpdated

--- src/pragmatic/homework/Exceptions/Student.java
+++ src/pragmatic/homework/Exceptions/Student.java
@@ -6,7 +6,9 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Student {
-	private boolean exception = true;	
+	private boolean exception = true;
+	private boolean NameException = true;
+	private boolean lastNameException = true;
 	private int age;
 	private String firstName;
 	private String lastName;
@@ -36,39 +38,48 @@ public class Student {
 	}
 
 	public void firstNameStudent() {
-		
 
+		do {
 			out.print("What is the first name of the student: ");
 			this.firstName = keyboard.next();
-			if (this.firstName.length() >= 12 || this.firstName.length() <= 1) {
-				do {
-				try {
-					exception = false;
-					throw new CustomException("First name cannot be longer than 12 Symbols or shorter than 1!!");
-					
-				}
 
-				catch (CustomException e) {
-					out.println("First name cannot be longer than 12 Symbols or shorter than 1!!");
+			try {
+				if (this.firstName.length() >= 12 || this.firstName.length() <= 1) {
+					throw new CustomException("First name cannot be longer than 12 Symbols or shorter than 1 !!");
 
 				}
-			}while (exception);
-		} 
+				NameException = false;
+			}
+
+			catch (CustomException e) {
+				out.println("First name cannot be longer than 12 Symbols or shorter than 1 !!");
+
+			}
+
+		} while (NameException);
 
 	}
 
 	public void lastNameStudent() {
 
-		out.print("What is the last name of the student: ");
-		this.firstName = keyboard.next();
-		if (this.firstName.length() >= 20) {
+		do {
+			out.print("What is the last name of the student: ");
+			this.firstName = keyboard.next();
+
 			try {
-				throw new CustomException("Last name cannot be longer than 20 Symbols!!");
-			} catch (CustomException e) {
-				out.println("First name cannot be longer than 12 Symbols or shorter than 1!!");
+				if (this.firstName.length() >= 20 || this.firstName.length() <= 1) {
+					throw new CustomException("Last name cannot be longer than 20 Symbols or shorter than 1 !!");
+
+				}
+				lastNameException = false;
+			}
+
+			catch (CustomException e) {
+				out.println("Last name cannot be longer than 12 Symbols or shorter than 1 !!");
 
 			}
-		}
+
+		} while (lastNameException);
 	}
 
 	// I do not know how to catch an exception here.
@@ -78,10 +89,10 @@ public class Student {
 		out.print("Is the student male or female?: ");
 		this.maleFemale = keyboard.next();
 
-		if (!maleFemale.equalsIgnoreCase("female")) {
+		if (maleFemale.equalsIgnoreCase("female")) {
 			out.println("Sex was set to Female");
 			this.isMale = false;
-			exception = false;
+
 		} else if (maleFemale.equalsIgnoreCase("male")) {
 			out.println("Sex was set to Male");
 			this.isMale = true;
