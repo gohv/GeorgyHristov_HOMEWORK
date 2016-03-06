69312a57004e5d3e678fc112f44711bdba43f518
gohv <hristovsotirovgeorgi@gmail.com>
2016-03-06 09:34:41 +0200

    PragmaticHomework

--- src/pragmatic/homework/dictionaryVER2/Index.java
+++ src/pragmatic/homework/dictionaryVER2/Index.java
@@ -1,12 +1,29 @@
 package pragmatic.homework.dictionaryVER2;
 
+import static java.lang.System.out;
+
+import pragmatic.homework.dictionary.DatabaseOfWords;
+
 public class Index {
+	private String EXIT = "EXIT";
 
 	public void addWord(String word, String explanation) {
 		Dictionary.dictionary.put(word, explanation);
 	}
-	public void search(String find){
+
+	public void search(String find) {
 		Dictionary.dictionary.get(find);
+		if (Dictionary.dictionary.get(find) == null) {
+			out.println("Nothing found! Search is Case sensitive!");
+		}
+	}
+
+	public void exit(String exit) {
+		this.EXIT = exit;
+		if (exit.equalsIgnoreCase("EXIT")) {
+			System.out.println("Bye Bye!");
+			System.exit(0);
+		}
 	}
 
 }
