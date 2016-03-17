50c1a2053eff72efba67eb8470e029d3e9350f1c
gohv <hristovsotirovgeorgi@gmail.com>
2016-03-17 22:17:59 +0200

    PragmaticHomework

--- src/pragmatic/homework/loginScreen/LoginView.java
+++ src/pragmatic/homework/loginScreen/LoginView.java
@@ -16,20 +16,25 @@ public class LoginView extends JFrame implements ActionListener {
 	private JTextField userName;
 	private JTextField passWord;
 	private JLabel userNameLabel = new JLabel("Username");
-	private JLabel passWordLabel;
+	private JLabel passWordLabel = new JLabel("Password");
 	private JTextField userNameField;
 	private JPasswordField passWordField;
 			
-	public void loginFrame(){
+	public LoginView(){
 		createUI();
 	}
 	
 	private void createUI(){
 		setTitle("LoginScreen");
-		setSize(3000, 1500);
+		setSize(350, 150);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLayout(null);
-		
+		// username Field		
+		userNameLabel.setBounds(10, 10, 80, 25);
+		add(userNameLabel);
+		//password field
+		passWordLabel.setBounds(10, 40, 80, 25);
+		add(passWordLabel);
 	}
 	
 	
